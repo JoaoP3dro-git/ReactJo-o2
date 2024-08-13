8fc3ce8134956d1e7f49211d5a54be5e2e0c4da7
João <veigarsemr@gmail.com>
2024-08-13 09:05:52 -0300

    c

--- joaoProva/src/components/PageContent.jsx
+++ joaoProva/src/components/PageContent.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function PageContent({ children }) {
     return (
-        <div className="flex-grow container mx-auto p-4">
+        <div className="flex-grow container mx-auto p-4 flex gap-4 min-w-full items-center justify-center">
             {children}
         </div>
     );
--- joaoProva/src/components/ProductCard.jsx
+++ joaoProva/src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 const ProductCard = ( product ) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black max-h-80">
       <img className="w-full h-48 object-cover" src={product.image} alt={product.title} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{product.title}</div>
@@ -8,7 +8,7 @@ const ProductCard = ( product ) => {
           {product.description}
         </p>
       </div>
-      <div className="px-6 py-4 flex items-center justify-between">
+      <div className="px-6 py-4 flex items-center justify-between gap-10">
         <span className="text-xl font-bold text-green-500">${product.price}</span>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Add to Cart
